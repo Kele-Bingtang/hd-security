23fa8ca9475a468f05b2e5641dfbfd7cac534170
kele_Bingtang <2456019588@qq.com>
2025-01-02 00:32:51 +0800

    优化 Redis 装配类代码

--- hd-security-plugin/hd-security-repository-redis/src/main/java/cn/youngkbt/security/redis/config/HdSecurityRedisRepositoryAutoConfiguration.java
+++ hd-security-plugin/hd-security-repository-redis/src/main/java/cn/youngkbt/security/redis/config/HdSecurityRedisRepositoryAutoConfiguration.java
@@ -48,13 +48,14 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
             throw new HdSecurityException("不支持该 Redis 序列化器：").setCode(HdSecurityRedisErrorCode.REDIS_SERIALIZER_NOT_SUPPORT);
         }
 
+        // 尝试获取 Hd Security 的 Redis 连接工厂
         RedisConnectionFactory connectionFactory = getHdSecurityRedisConnectionFactory(environment);
 
         return serializer.getRepositoryFunction().apply(Optional.ofNullable(connectionFactory).orElse(redisConnectionFactory));
     }
 
     /**
-     * 获取在配置文件 hd-security.redis 的 Redis 信息来创建 Redis 连接工厂（业务数据和 Hd Security 认证数据分离时需要进行配置）
+     * 获取在配置文件 hd-security.redis 的 Redis 配置信息来创建 Redis 连接工厂（业务数据和 Hd Security 认证数据分离时需要进行配置）
      *
      * @param environment 环境变量
      * @return Redis 连接工厂
@@ -67,17 +68,17 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
 
         switch (pattern) {
             case "single" -> {
-                return getLettuceConnectionFactory(getSingleRedisConfiguration(redisProperties), redisProperties);
+                return createLettuceConnectionFactory(createSingleRedisConfiguration(redisProperties), redisProperties);
             }
             case "cluster" -> {
-                return getLettuceConnectionFactory(getClusterRedisConfiguration(redisProperties), redisProperties);
+                return createLettuceConnectionFactory(createClusterRedisConfiguration(redisProperties), redisProperties);
             }
             case "sentinel" -> {
-                return getLettuceConnectionFactory(getSentinelRedisConfiguration(redisProperties), redisProperties);
+                return createLettuceConnectionFactory(createSentinelRedisConfiguration(redisProperties), redisProperties);
             }
             case "socket" -> {
                 String socket = environment.getProperty(REDIS_PREFIX + ".socket", "");
-                return getLettuceConnectionFactory(getSocketRedisConfiguration(redisProperties, socket), redisProperties);
+                return createLettuceConnectionFactory(createSocketRedisConfiguration(redisProperties, socket), redisProperties);
             }
         }
 
@@ -90,13 +91,15 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
      * @param redisProperties 基础配置信息
      * @return Redis 单体模式的配置信息
      */
-    public RedisConfiguration getSingleRedisConfiguration(RedisProperties redisProperties) {
+    public RedisConfiguration createSingleRedisConfiguration(RedisProperties redisProperties) {
         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+
         redisConfig.setHostName(redisProperties.getHost());
         redisConfig.setPort(redisProperties.getPort());
         redisConfig.setDatabase(redisProperties.getDatabase());
         redisConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
         redisConfig.setDatabase(redisProperties.getDatabase());
+
         // 低版本没有 username 属性，捕获异常给个提示即可，无需退出程序
         try {
             redisConfig.setUsername(redisProperties.getUsername());
@@ -112,23 +115,25 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
      * @param redisProperties 基础配置信息
      * @return Redis 普通集群的配置信息
      */
-    public RedisConfiguration getClusterRedisConfiguration(RedisProperties redisProperties) {
+    public RedisConfiguration createClusterRedisConfiguration(RedisProperties redisProperties) {
         RedisClusterConfiguration redisClusterConfig = new RedisClusterConfiguration();
+        RedisProperties.Cluster cluster = redisProperties.getCluster();
+
+        List<RedisNode> serverList = cluster.getNodes().stream().map(node -> {
+            String[] ipAndPort = node.split(":");
+            return new RedisNode(ipAndPort[0].trim(), Integer.parseInt(ipAndPort[1]));
+        }).collect(Collectors.toList());
+
+        redisClusterConfig.setClusterNodes(serverList);
+        redisClusterConfig.setMaxRedirects(cluster.getMaxRedirects());
+        redisClusterConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
+
         // 低版本没有 username 属性，捕获异常给个提示即可，无需退出程序
         try {
             redisClusterConfig.setUsername(redisProperties.getUsername());
         } catch (NoSuchMethodError e) {
             System.err.println(e.getMessage());
         }
-        redisClusterConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
-
-        RedisProperties.Cluster cluster = redisProperties.getCluster();
-        List<RedisNode> serverList = cluster.getNodes().stream().map(node -> {
-            String[] ipAndPort = node.split(":");
-            return new RedisNode(ipAndPort[0].trim(), Integer.parseInt(ipAndPort[1]));
-        }).collect(Collectors.toList());
-        redisClusterConfig.setClusterNodes(serverList);
-        redisClusterConfig.setMaxRedirects(cluster.getMaxRedirects());
         return redisClusterConfig;
     }
 
@@ -138,26 +143,28 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
      * @param redisProperties 基础配置信息
      * @return Redis 哨兵模式的配置信息
      */
-    public RedisConfiguration getSentinelRedisConfiguration(RedisProperties redisProperties) {
-        // 哨兵集群模式
+    public RedisConfiguration createSentinelRedisConfiguration(RedisProperties redisProperties) {
         RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration();
         redisSentinelConfiguration.setDatabase(redisProperties.getDatabase());
+
+        RedisProperties.Sentinel sentinel = redisProperties.getSentinel();
+
+        List<RedisNode> serverList = sentinel.getNodes().stream().map(node -> {
+            String[] ipAndPort = node.split(":");
+            return new RedisNode(ipAndPort[0].trim(), Integer.parseInt(ipAndPort[1]));
+        }).collect(Collectors.toList());
+
+        redisSentinelConfiguration.setSentinels(serverList);
+        redisSentinelConfiguration.setMaster(sentinel.getMaster());
+        redisSentinelConfiguration.setSentinelPassword(sentinel.getPassword());
+        redisSentinelConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+
         // 低版本没有 username 属性，捕获异常给个提示即可，无需退出程序
         try {
             redisSentinelConfiguration.setUsername(redisProperties.getUsername());
         } catch (NoSuchMethodError e) {
             System.err.println(e.getMessage());
         }
-        redisSentinelConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
-
-        RedisProperties.Sentinel sentinel = redisProperties.getSentinel();
-        redisSentinelConfiguration.setMaster(sentinel.getMaster());
-        redisSentinelConfiguration.setSentinelPassword(sentinel.getPassword());
-        List<RedisNode> serverList = sentinel.getNodes().stream().map(node -> {
-            String[] ipAndPort = node.split(":");
-            return new RedisNode(ipAndPort[0].trim(), Integer.parseInt(ipAndPort[1]));
-        }).collect(Collectors.toList());
-        redisSentinelConfiguration.setSentinels(serverList);
 
         return redisSentinelConfiguration;
     }
@@ -168,33 +175,35 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
      * @param redisProperties 基础配置信息
      * @return Redis Socket 连接模式的配置信息
      */
-    public RedisConfiguration getSocketRedisConfiguration(RedisProperties redisProperties, String socket) {
-        // socket 连接单体 Redis
+    public RedisConfiguration createSocketRedisConfiguration(RedisProperties redisProperties, String socket) {
         RedisSocketConfiguration redisSocketConfiguration = new RedisSocketConfiguration();
+
         redisSocketConfiguration.setDatabase(redisProperties.getDatabase());
+        redisSocketConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        redisSocketConfiguration.setSocket(socket);
+
         // 低版本没有 username 属性，捕获异常给个提示即可，无需退出程序
         try {
             redisSocketConfiguration.setUsername(redisProperties.getUsername());
         } catch (NoSuchMethodError e) {
             System.err.println(e.getMessage());
         }
-        redisSocketConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
-        redisSocketConfiguration.setSocket(socket);
 
         return redisSocketConfiguration;
     }
 
     /**
-     * 获取 Lettuce 连接工厂
+     * 创建 Lettuce 连接工厂
      *
      * @param redisConfiguration Redis 配置信息
      * @param redisProperties    Redis 基础配置信息
      * @return Lettuce 连接工厂
      */
-    public LettuceConnectionFactory getLettuceConnectionFactory(RedisConfiguration redisConfiguration, RedisProperties redisProperties) {
+    public LettuceConnectionFactory createLettuceConnectionFactory(RedisConfiguration redisConfiguration, RedisProperties redisProperties) {
         GenericObjectPoolConfig<RedisConfiguration> poolConfig = new GenericObjectPoolConfig<>();
-        // pool 配置 
+        // Redis 连接池配置 
         RedisProperties.Lettuce lettuce = redisProperties.getLettuce();
+        
         if (null != lettuce.getPool()) {
             RedisProperties.Pool pool = redisProperties.getLettuce().getPool();
             // 连接池最大连接数
@@ -208,13 +217,12 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
         }
         LettucePoolingClientConfiguration.LettucePoolingClientConfigurationBuilder builder = LettucePoolingClientConfiguration.builder();
 
-        // timeout 
-        if (redisProperties.getTimeout() != null) {
+        // 设置 Redis 命令执行过期时间 
+        if (null != redisProperties.getTimeout()) {
             builder.commandTimeout(redisProperties.getTimeout());
         }
 
-        // shutdownTimeout 
-        if (lettuce.getShutdownTimeout() != null) {
+        if (null != lettuce.getShutdownTimeout()) {
             builder.shutdownTimeout(lettuce.getShutdownTimeout());
         }
 
@@ -225,5 +233,4 @@ public class HdSecurityRedisRepositoryAutoConfiguration {
 
         return factory;
     }
-
 }
